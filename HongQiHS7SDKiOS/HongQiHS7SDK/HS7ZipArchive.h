5cbee4a569f3f6636e1919da4095ec2d1b64b51b
张三 <zhangsan0103@gmail.com>
2019-03-25 16:43:32 +0800

    0.0.3 test

--- HongQiHS7SDKiOS/HongQiHS7SDK/HS7ZipArchive.h
+++ HongQiHS7SDKiOS/HongQiHS7SDK/HS7ZipArchive.h
@@ -16,5 +16,5 @@ FOUNDATION_EXPORT const unsigned char ZipArchiveVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ZipArchive/PublicHeader.h>
 
-#import "SSZipArchive.h"
+#import "HS7SSZipArchive.h"
 
